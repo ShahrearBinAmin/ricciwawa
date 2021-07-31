95b5bc549e9f40045e13b303310d3c4e21b711eb
Shahrear <shahrearbinamin1@gmail.com>
2021-08-01 02:04:54 +0600

    refactored stat screen

--- lib/screens/statistics/statistics_screen.dart
+++ lib/screens/statistics/statistics_screen.dart
@@ -32,53 +32,16 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
         child: Container(
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Text("Q${_question.position}. ${_question.questionText}",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17)),
-              ),
+              QuestionText(question: _question),
               ListView.separated(
                   shrinkWrap: true,
                   itemBuilder: (BuildContext context, int index) {
                     return Column(
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: OptionSelectorDetails(
-                            option: _question.options[index],
-                          ),
+                        OptionStats(
+                          option: _question.options[index],
                         ),
-                        Container(
-                          margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                          color: Color(0xFF262626),
-                          child: ListView.separated(
-                              shrinkWrap: true,
-                              itemBuilder:
-                                  (BuildContext context, int innerIndex) {
-                                var user = _question.options[index].optionStat
-                                    .answeredUsers[innerIndex];
-                                return ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundColor: kLightGreyColor,
-                                  ),
-                                  title: Text(
-                                    user.name,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                );
-                              },
-                              separatorBuilder:
-                                  (BuildContext context, int index) => SizedBox(
-                                        height: 0,
-                                      ),
-                              itemCount: _question.options[index].optionStat
-                                  .answeredUsers.length),
-                        )
+                        UserList(option: _question.options[index])
                       ],
                     );
                   },
@@ -95,27 +58,86 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
   }
 }
 
-class OptionSelectorDetails extends StatelessWidget {
+class QuestionText extends StatelessWidget {
+  const QuestionText({
+    Key? key,
+    required Question question,
+  })  : _question = question,
+        super(key: key);
+
+  final Question _question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Text("Q${_question.position}. ${_question.questionText}",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 17)),
+    );
+  }
+}
+
+class UserList extends StatelessWidget {
+  const UserList({
+    Key? key,
+    required Option option,
+  })  : _option = option,
+        super(key: key);
+
+  final Option _option;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+      color: Color(0xFF262626),
+      child: ListView.separated(
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int innerIndex) {
+            var user = _option.optionStat.answeredUsers[innerIndex];
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundColor: kLightGreyColor,
+              ),
+              title: Text(
+                user.name,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => SizedBox(
+                height: 0,
+              ),
+          itemCount: _option.optionStat.answeredUsers.length),
+    );
+  }
+}
+
+class OptionStats extends StatelessWidget {
   final Option option;
-  const OptionSelectorDetails({Key? key, required this.option})
-      : super(key: key);
+  const OptionStats({Key? key, required this.option}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var optionStat = option.optionStat;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          "${option.optionText}-${optionStat.percentage.round()}%",
-          style: TextStyle(color: Colors.white, fontSize: 16),
-        ),
-        Text(
-          "${optionStat.answeredUsersCount} Votes",
-          style: TextStyle(color: Colors.white, fontSize: 16),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "${option.optionText}-${optionStat.percentage.round()}%",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          Text(
+            "${optionStat.answeredUsersCount} Votes",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ],
+      ),
     );
   }
 }
